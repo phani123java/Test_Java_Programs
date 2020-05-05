076d2bde4cb4684e17aef3ce3c30b68802b2fd8b
MPHANIRAJKIRAN <MPHANIRAJKIRAN@PHANIRAJKIRAN>
2020-05-05 13:58:03 +0530

    more simple expression example

---
+++ SimpleExpressions/src/SimpleExpressions_more.java
@@ -0,0 +1,17 @@
+public class SimpleExpressions_more
+{
+ public static void main(String[] args)
+ {
+ int counter = 30;
+ int counter2 = counter;
+ System.out.println(counter);
+ short s = 20;
+ System.out.println(s);
+ int i = s;
+ System.out.println(i);
+ // short s2 = 40000; // possible loss of precision error
+ int i2 = -1;
+ double d = i2;
+ System.out.println(d);
+ }
+}
