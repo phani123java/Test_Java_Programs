74f61eab0178605b5c10ae5b69f329bc1ae3ab3e
MPHANIRAJKIRAN <MPHANIRAJKIRAN@PHANIRAJKIRAN>
2020-05-05 16:57:49 +0530

    JDK 14.0.1 JRE 13

---
+++ CompundExpressions/src/Bitwise.java
@@ -0,0 +1,10 @@
+public class Bitwise
+{
+ public static void main(String[] args)
+ {
+ System.out.println(~181);
+ System.out.println(26 & 183);
+ System.out.println(26 ^ 183);
+ System.out.println(26 | 183);
+ }
+}
