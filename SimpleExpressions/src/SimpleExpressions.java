dccf7345b005489d03d4fcc2021fb92adc7b203c
MPHANIRAJKIRAN <MPHANIRAJKIRAN@PHANIRAJKIRAN>
2020-05-05 13:53:09 +0530

    Simple Expressions

---
+++ SimpleExpressions/src/SimpleExpressions.java
@@ -0,0 +1,15 @@
+
+public class SimpleExpressions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int counter =10;
+		double pie = 3.1415926;
+		char testEncode = 'p';
+		System.out.println(counter);
+		System.out.println(pie);
+		System.out.println(testEncode);
+
+	}
+
+}
